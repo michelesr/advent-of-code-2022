690080e1a12ed35fa667727ba13643441ac467e2
Michele Sorcinelli <michelesr@autistici.org>
2022-12-14 21:56:56 +0000

    Refactor Day-14 to use a grid

--- day-14/main.py
+++ day-14/main.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def up(start_x: int, start_y: int, end_x: int, end_y: int) -> list[tuple[int, int]]:
     return [(start_x, n) for n in range(start_y, end_y - 1, -1)]
 
@@ -30,50 +32,43 @@ def get_points(
     raise ValueError(f"invalid range: {(start_x, start_y)} -> {(end_x, end_y)}")
 
 
-def go(max_depth: int, rocks: list[tuple[int, int]], part_two: bool = False) -> int:
+def go(max_depth: int, rocks: list[list[bool]], part_two: bool = False) -> int:
     steps = 0
     while True:
         # rock starts
         a, b = (500, 0)
 
         end = False
-        res = None
         while True:
-            if (not part_two and b > max_depth) or (part_two and (500, 0) in rocks):
+            if (not part_two and b > max_depth) or (part_two and rocks[500][0]):
                 end = True
                 break
 
             if part_two and b > max_depth:
-                res = a, b
                 break
 
             # down?
-            if (a, b + 1) not in rocks:
+            if not rocks[a][b + 1]:
                 b += 1
                 continue
 
             # left?
-            elif (a - 1, b + 1) not in rocks:
+            elif not rocks[a - 1][b + 1]:
                 a, b = a - 1, b + 1
 
             # right?
-            elif (a + 1, b + 1) not in rocks:
+            elif not rocks[a + 1][b + 1]:
                 a, b = a + 1, b + 1
 
             else:
-                res = a, b
                 break
-
         if end:
             break
 
-        if not res:
-            raise Exception("res not set")
-
-        rocks.append(res)
+        rocks[a][b] = True
         if not part_two:
             # in part two max_depth is static
-            max_depth = max(max_depth, res[1])
+            max_depth = max(max_depth, b)
         steps += 1
     return steps
 
@@ -96,10 +91,13 @@ for path in paths:
         x, y = int(x), int(y)
         rocks += get_points(x, y, a, b)
 
+grid = [[False for i in range(1000)] for j in range(1000)]
+for a, b in rocks:
+    grid[a][b] = True
 
 # part one
 max_depth = max((r[1] for r in rocks))
-print(go(max_depth, rocks.copy()))
+print(go(max_depth, deepcopy(grid)))
 
 # part two
-print(go(max_depth, rocks.copy(), part_two=True))
+print(go(max_depth, deepcopy(grid), part_two=True))
